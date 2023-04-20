8f7f3d0b016d134d46d3220ec3d7b58a5871315c
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-04-20 19:57:45 +0400

    Create Project

---
+++ app/src/main/java/com/azimzada/counterapplication/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.azimzada.counterapplication.activity
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import com.azimzada.counterapplication.R
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
